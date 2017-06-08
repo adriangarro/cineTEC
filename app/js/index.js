997e78d6492cd2443dac6ad94911c8ea655be48f
Adrian <adriangarro@icloud.com>
2017-06-08 14:01:32 -0600

    Adding possibility of seeing branches

--- app/js/index.js
+++ app/js/index.js
@@ -6,6 +6,7 @@
 	____________________________________
 */
 
+// connect to firebase
 var config = {
 	apiKey: "AIzaSyBPFPZngjPBsG38peTcXUP8rWA8QnrUanQ",
 	authDomain: "cinetec-b2a0f.firebaseapp.com",
@@ -14,24 +15,60 @@ var config = {
 	storageBucket: "cinetec-b2a0f.appspot.com",
 	messagingSenderId: "171738294119"
 };
-
 firebase.initializeApp(config);
 
-$(function showCountries(){
+function showCountries() {
 	// create a new connection to firebase
-	var refCountries = firebase.database().ref("countries").orderByKey();
+	var refCountries = firebase.database()
+		.ref("countries")
+		.orderByKey();
 	// listen to data updates from firebase
 	refCountries.once("value")
-		.then(function(snapshot){
+		.then(function(snapshot) {
 			snapshot.forEach(function(childSnapshot) {
 				var countryKey = childSnapshot.key;
 				var countryData = childSnapshot.val();
 				var countryOption = "<option value=" + countryKey + ">" 
-					+ countryData.name + "</option>"
+					+ countryData.name + "</option>";
 				$(countryOption).appendTo("#selectCountry");
-				// console.log(countryKey);
-				// console.log(countryData.name);
 			});
 		});
-})
+}
+
+function showBranches() {
+	$("#selectCountry")
+		.change(function() {
+			// clean branch options
+			$("#selectBranch")
+				.html("");
+			// adding dummy option
+			$("<option disabled selected value>Sucursal</option>")
+				.appendTo("#selectBranch");
+			// preparing path
+			var branchesPath = "branches/";
+			$("#selectCountry option:selected").each(function() {
+				branchesPath += $(this).val();
+				// create a new connection to firebase
+				var refBranches = firebase.database()
+					.ref(branchesPath)
+					.orderByKey();	
+				// listen to data updates from firebase	
+				refBranches.once("value")
+					.then(function(snapshot) {
+						snapshot.forEach(function(childSnapshot) {
+							var branchKey = childSnapshot.key;
+							var branchData = childSnapshot.val();
+							var branchOption = "<option value=" + branchKey + ">" 
+								+ branchData.name + "</option>";
+							$(branchOption).appendTo("#selectBranch");
+						});
+					});
+			});
+		});
+}
+
+jQuery(
+	showCountries(),
+	showBranches()
+);
 
